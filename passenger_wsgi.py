4ad5867a3b5b2574e8fabfd53e78a25a909afd34
Santosh Shrestha <santoshshrestha@Santoshs-MacBook-Air.local>
2026-01-03 20:56:23 +0545

    Fix main py of the productions

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -3,7 +3,7 @@ import sys
 
 sys.path.insert(0, os.path.dirname(__file__))
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zorpido_config.settings.production")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
