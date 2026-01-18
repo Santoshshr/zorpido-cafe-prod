290747af580fb282ce3c6d36247df7a3878604a7
Santosh Shrestha <santoshshrestha@Santoshs-MacBook-Air.local>
2026-01-18 23:08:15 +0545

    update build.sh

--- manage.py
+++ manage.py
@@ -2,10 +2,16 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE",
-        "zorpido_config.settings.local"
-    )
+    if os.environ.get('RENDER'):
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE",
+            "zorpido_config.settings.production"
+        )
+    else:
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE",
+            "zorpido_config.settings.local"
+        )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
